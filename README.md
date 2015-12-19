cd4f7187d3511f6c0c24ecfa8b560535ea14ece2
Jay Liang <roackb2@gmail.com>
2015-12-19 11:58:06 +0800

    complete version 1

---
+++ README.md
@@ -0,0 +1,154 @@
+# Intro
+
+   Stringify makes you easy to stringify complex data structure,
+ especially useful for debugging purpose. Since object may implement its own toJSON function,
+ JSON.stringify may ignore some attributes. Using Stringify, you don't need to worrty that something is hidden
+ from custom toJSON implementation. Also, Stringify will print functions if there's any one,
+ and you may choose to print the content of the functions.
+
+# Installation
+
+```bash
+npm install git+https://git@github.com/roackb2/stringify.git
+```
+
+# Feature
+
+* Circular detection: No need to worry about circular refrences
+* Function attributes printed: You could see all functions inside an object
+* Print function content: If specified, Stringify would print function content and is beatifully indented. (as long as the source code of the function is correctly indented)
+
+# Usage
+
+```javascript
+var stringify = require('stringify')
+
+var obj = [{
+    hello: "world1",
+    test: true,
+}, {
+
+    hello: "world2",
+    test: {
+        inner: 1, object: "two"
+    },
+    test2: {
+        inner: 2, object: "three"
+    }
+}, {
+    hello: "world3",
+    test: [{
+        inner: 1
+    }, {
+        inner: {
+            supperInner: "hi",
+            func: function() {
+                var test;
+            },
+            func2: function() {
+                var test2
+            },
+            err: new Error("test error object"),
+            date: new Date(),
+            reg: /ab+c/
+        }
+    }]
+}];
+obj[3] = obj;
+obj[4] = obj[1];    var args;
+function test(one, two, three) {
+    obj[5] = arguments;
+    console.log(stringify(obj, false, true));
+}
+test(1, 2, 3)
+```
+The result would be:
+
+```javascript
+[
+    0: {
+        hello: world1,
+        test: true
+    },
+    1: {
+        hello: world2,
+        test: {
+            inner: 1,
+            object: two
+        },
+        test2: {
+            inner: 2,
+            object: three
+        }
+    },
+    2: {
+        hello: world3,
+        test: [
+            0: {
+                inner: 1
+            },
+            1: {
+                inner: {
+                    supperInner: hi,
+                    func: function () {
+                        var test;
+                    },
+                    func2: function () {
+                        var test2
+                    },
+                    err: Error: test error object,
+                    date: Sat Dec 19 2015 11:50:55 GMT+0800 (CST),
+                    reg: /ab+c/
+                }
+            }
+        ]
+    },
+    3: [Circular],
+    4: {
+        hello: world2,
+        test: {
+            inner: 1,
+            object: two
+        },
+        test2: {
+            inner: 2,
+            object: three
+        }
+    },
+    5: [
+        0: 1,
+        1: 2,
+        2: 3
+    ]
+]
+```
+
+# Parameters
+
+stringify(obj, ignoreFunc, printFuncContent)
+
+* obj [Any]: the object to stringify
+* ignoreFunc [boolean]: whether to ignore function attributes
+* printFuncContent [boolean]: whether to print funciton content, the ignoreFunc must be set to false.
+
+# Supported Data Types
+
+* Object
+* Array
+* Arguments
+* Date
+* Boolean
+* Number
+* String
+
+# Unsupported Data Types
+
+* Map
+* Set
+* WeakMap
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,103 @@
+module.exports = stringify;
+
+function stringify(obj, ignoreFunc, printFuncContent, former, depth, path) {
+    var indent = "    ";
+    if (ignoreFunc == undefined) {
+        ignoreFunc = false;
+    }
+    if (printFuncContent == undefined) {
+        printFuncContent = false;
+    }
+    if (former == undefined) {
+        former = "";
+    }
+    if (depth == undefined) {
+        depth = 0;
+    }
+    if (path == undefined) {
+        path = [];
+    }
+
+    var prefix = "" + new Array(depth + 1).join(indent);
+    var encountered = (path.indexOf(obj) !== -1);
+    path.push(obj);
+
+    var result = former;
+    if (Object.prototype.toString.call(obj) === '[object Array]' || Object.prototype.toString.call(obj) === '[object Arguments]') {
+        if (!encountered) {
+            if(obj.length > 0) {
+                result += "[\n";
+                for (var i = 0; i < obj.length; i++) {
+                    result += indent + prefix + i + ": ";
+                    result += stringify(obj[i], ignoreFunc, printFuncContent, "", depth + 1, [].concat(path));
+                    if (i < obj.length - 1) {
+                        result += ",\n";
+                    } else {
+                        result += "\n";
+                    }
+                }
+                result += prefix + "]";
+            } else {
+                result += "[ ]";
+            }
+        } else {
+            result += "[Circular]";
+        }
+    } else if (Object.prototype.toString.call(obj) === '[object Object]') {
+        if (!encountered) {
+            var count = 0;
+            var totalCount = 0;
+            for (var key in obj) {
+                totalCount++;
+                if (Object.prototype.toString.call(obj[key]) === '[object Function]' || !ignoreFunc) {
+                    count++;
+                }
+            }
+            if(totalCount > 0) {
+                result += "{\n";
+                var counter = 0;
+                for (var key in obj) {
+                    var value = obj[key];
+                    if (Object.prototype.toString.call(value) === '[object Function]' || !ignoreFunc) {
+                        counter++;
+                        result += prefix + indent + key + ": " + stringify(value, ignoreFunc, printFuncContent, "", depth + 1, [].concat(path));
+                        if (counter < count) {
+                            result += ",\n";
+                        } else {
+                            result += "\n";
+                        }
+                    }
+                }
+                result += prefix + "}"
+            } else {
+                result += "{}";
+            }
+
+        } else {
+            result += "[Circular]"
+        }
+    } else if (Object.prototype.toString.call(obj) === '[object Function]') {
+        if (printFuncContent) {
+            var lines = ("" + obj).split("\n");
+            var funcContent = "";
+            var lastLine = lines[lines.length - 1];
+            var lasIndent = lastLine.replace(lastLine.trim(), "")
+            for(var i = 0; i < lines.length; i++) {
+                if (i === 0) {
+                    funcContent += lines[i];
+                } else {
+                    funcContent += prefix + lines[i].substring(lasIndent.length, lines[i].length);
+                }
+                if (i !== lines.length - 1) {
+                    funcContent += "\n";
+                }
+            }
+            result += funcContent;
+        } else {
+            result += "_function_";
+        }
+    } else {
+        result += obj + "";
+    }
+    return result;
+}
